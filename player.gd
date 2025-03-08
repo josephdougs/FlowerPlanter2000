580d6903ee533170ef50e31dfa53bae03b264807
Joey Smith <josephdougs@gmail.com>
2025-03-08 05:40:07 -0800

    Add basic bugged out movement

--- player.gd
+++ player.gd
@@ -3,22 +3,23 @@ extends Node3D
 var viewport: Window
 var camera: Camera3D
 
+var MOVE_AMOUNT = 3
+var ROTATE_AMOUNT = 0.5
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	viewport = get_viewport()
 	camera = $"./Camera3D"
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-	
-	
-	
-func _input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == 1 && event.is_pressed():
-			pass
-			#print(viewport.get_mouse_position())
-			#print(camera.project_local_ray_normal(viewport.get_mouse_position()))
+	print(delta)
+	if Input.is_action_pressed("up"):
+		position.z += (MOVE_AMOUNT * delta)
+	if Input.is_action_pressed("down"):
+		position.z -= (MOVE_AMOUNT * delta)
+	if Input.is_action_pressed("left"):
+		rotation.y += (ROTATE_AMOUNT * delta)
+	if Input.is_action_pressed("right"):
+		rotation.y -= (ROTATE_AMOUNT * delta)
